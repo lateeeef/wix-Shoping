ece8ec47ddb0dd2626d3364b8831a32e21733f30
Abdulateef-001 <olalekanoluwa2@gmail.com>
2022-11-13 11:11:29 +0100

    first commit

---
+++ changeimage.js
@@ -0,0 +1,5 @@
+function changeImage(){
+    document.getElementById('box-1').innerHTML="<img src='https://m.media-amazon.com/images/I/41gBQCKQzCS._AC_US40_.jpg'>"
+}
+let x = document.getElementById('box-1')
+x.style.background="black"
